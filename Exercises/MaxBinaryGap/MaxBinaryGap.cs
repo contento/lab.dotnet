8374b935aeac3e7e294c3889b3c2519db8bd8c9c
Gonzalo Contento <gonzalo@conten.to>
2018-11-25 23:38:33 -0500

    Best solution I can come up with

--- Exercises/MaxBinaryGap/MaxBinaryGap.cs
+++ Exercises/MaxBinaryGap/MaxBinaryGap.cs
@@ -7,21 +7,21 @@ namespace Exercise
         public static void Main(string[] args)
         {
             int[] values = {
-                805306373 
-                // 1
-                // ,5
-                // ,16
-                // ,19
-                // ,1024
-                // ,1162
-                // ,66561
-                // ,561892
-                // ,6291457
-                // ,74901729
-                // ,805306373
-                // ,1073741825
-                // ,1610612737
-                // ,2147483647
+                1
+                ,5
+                ,9
+                ,16
+                ,19
+                ,1024
+                ,1162
+                ,66561
+                ,561892
+                ,6291457
+                ,74901729
+                ,805306373
+                ,1073741825
+                ,1610612737
+                ,2147483647
             };
 
             foreach (var n in values)
@@ -32,49 +32,47 @@ namespace Exercise
 
         public static void ExecuteSolution(int n)
         {
-            Console.Write($"{n} [{Convert.ToString(n, 2)}] = > ");
+            Console.Write($"{n} [{Convert.ToString(n, 2)}] => ");
             Console.WriteLine($"{Solution(n)}");
         }
 
-        public static int Solution(int N)
+        public static int Solution(int n)
         {
             int gap = 0;
             int maxGap = 0;
             bool potentialGap = false;
             bool inGap = false;
+            // var binary = Convert.ToString(n, 2); // DEBUG
 
-            while (N != 0)
+            while (n != 0)
             {
-                bool bit = (N % 2) == 1;
-                if (!bit && inGap)
+                bool bit = (n % 2) == 1;
+
+                if (bit)
                 {
-                    gap++;
-                }
-                else if (bit && inGap)
-                {
-                    if (gap > maxGap)
+                    if (inGap)
                     {
-                        maxGap = gap;
+                        if (gap > maxGap)
+                            maxGap = gap;
+                        gap = 0;
+                        inGap = false;
                     }
-                    gap = 0;
-                    inGap = false;
+                    if (!potentialGap)
+                        potentialGap = true;
                 }
-                else if (bit && !inGap)
+                else
                 {
-                    potentialGap = true;
-                }
-                else if (!bit && potentialGap)
-                {
-                    gap = 1;
-                    inGap = true;
-                    potentialGap = false;
-                }
-                else if (bit && !potentialGap)
-                {
-                    potentialGap = true;
+                    if (inGap)
+                        gap++;
+                    else if (potentialGap)
+                    {
+                        gap = 1;
+                        inGap = true;
+                        potentialGap = false;
+                    }
                 }
 
-                N = N >> 1;
+                n = n >> 1;
             }
 
             return maxGap;

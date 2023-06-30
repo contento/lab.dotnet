9585c40d3897e1ec4977c2cac039ca1dc52582c3
Gonzalo Contento <contento@outlook.com>
2023-06-30 13:56:19 -0400

    Working 100%

--- Cryptography/CryptographyTester/MainForm.cs
+++ Cryptography/CryptographyTester/MainForm.cs
@@ -2,14 +2,11 @@ using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Encodings;
 using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Modes;
 using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.Pkcs;
 using Org.BouncyCastle.Security;
-using Org.BouncyCastle.X509;
 
 namespace CryptographyTester
 {

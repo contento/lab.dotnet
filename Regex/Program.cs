4a46b901899b3753b4987f0e184b096544d59227
Gonzalo Contento <gonzalo.contento@micronotes.com>
2024-02-21 15:25:52 -0500

    Regex tests

---
+++ Regex/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"\b[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\b";
+
+string input = "Here is an email address: example@test.com. Let's find it!";
+
+Regex r = new(pattern, RegexOptions.IgnoreCase);
+
+Match m = r.Match(input);
+while (m.Success)
+{
+    Console.WriteLine("Found '{0}' at position {1}.", m.Value, m.Index);
+    m = m.NextMatch();
+}

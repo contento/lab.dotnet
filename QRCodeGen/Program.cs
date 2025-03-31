3d4da84ec0a959044fa4a46357163f470b0a543a
Gonzalo Contento <3741250+contento@users.noreply.github.com>
2025-03-30 20:40:01 -0400

    Upgrade to .NET 4.8 and .NET 9

--- QRCodeGen/Program.cs
+++ QRCodeGen/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+using QRCoder;
+
+namespace QRCodeGen;
+
+public class Program
+{
+    public static void Main()
+    {
+        const string filePath = "Assets/urls.csv";
+        const string baseUrl = "https://gc2.me";
+        const string outputDirectory = "Assets";
+
+        var urls = LoadUrls(filePath);
+        var enabledUrls = urls.Where(url => url.Enabled);
+
+        foreach (var url in enabledUrls)
+        {
+            var shortUrl = $"{baseUrl}/{url.Short}";
+            GenerateAndSaveQrCode(shortUrl, url.Short, outputDirectory);
+        }
+    }
+
+    static IEnumerable<UrlEntry> LoadUrls(string filePath)
+    {
+        var lines = File.ReadAllLines(filePath);
+
+        return lines.Skip(1).Select(line => line.Split(',')).Select(columns => new UrlEntry { Enabled = bool.Parse(columns[0]), Short = columns[1], Long = columns[2], Description = columns[3] }).ToList();
+    }
+
+    private static void GenerateAndSaveQrCode(string url, string shortName, string outputDirectory)
+    {
+        var qrGenerator = new QRCodeGenerator();
+        var qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+        using (var qrCode = new QRCode(qrCodeData))
+        {
+            using (var qrCodeImage = qrCode.GetGraphic(20))
+            {
+                var filePath = Path.Combine(outputDirectory, $"{shortName}.png");
+                qrCodeImage.Save(filePath, ImageFormat.Png);
+            }
+        }
+    }
+}
+
+internal class UrlEntry
+{
+    public required bool Enabled { get; set; }
+    public required string Short { get; set; }
+    public required string Long { get; set; }
+    public required string Description { get; set; }
+}
